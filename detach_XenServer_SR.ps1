dc920d7239f59b31461630e195cf2ac11ee6e73d
gjozic <gjozic@live.com>
2016-07-15 17:43:32 -0400

    added credential prompt

--- detach_XenServer_SR.ps1
+++ detach_XenServer_SR.ps1
@@ -1,4 +1,12 @@
-﻿$a = 'NFS ISO library'
+﻿$master = Read-Host "Enter Pool Master name"
+$prompt = Get-Credential
+$xenserver_password = $prompt.Password
+$xenserver_username = $prompt.UserName
+$xenserver_credential = New-Object -Typename System.Management.Automation.PSCredential -ArgumentList $xenserver_username, $xenserver_password
+Connect-XenServer -Server $master -Creds $xenserver_credential -SetDefaultSession -NoWarnCertificates
+
+
+$a = 'NFS ISO library'
 $b = Get-XenSR -Name $a
 foreach ($pbd in $b.PBDs)
 

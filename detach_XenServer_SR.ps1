9a83234300c0c30ea012bd8a18a39a909368480d
gjozic <gjozic@live.com>
2016-07-15 16:32:44 -0400

    Initial commit
    Changes to be committed:
    	new file:   detach_XenServer_SR.ps1

---
+++ detach_XenServer_SR.ps1
@@ -0,0 +1,11 @@
+﻿$a = 'NFS ISO library'
+$b = Get-XenSR -Name $a
+foreach ($pbd in $b.PBDs)
+
+{
+    $x = Get-XenPBD -Ref $pbd 
+   
+    Invoke-XenPBD -PBD $x -XenAction Unplug -Verbose
+     
+}
+Invoke-XenSR -SR $b -XenAction Forget
